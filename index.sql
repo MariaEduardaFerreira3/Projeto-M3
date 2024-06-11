9e9d9f62783d5888cbcdc178fa5a2e2e717bb29e
Cristhyan Lima <cristhyanlima768@gmail.com>
2024-06-11 17:38:55 -0300

    Updates nas tabelas

--- index.sql
+++ index.sql
@@ -18,10 +18,10 @@ ALTER TABLE tb_company ADD FOREIGN KEY (supplier_id) REFERENCES tb_supplier(id);
 -- SABRINA: Company e Supplier
 INSERT INTO tb_company (cnpj, name, location, stock_id, supplier_id) VALUES
 ('12345678000190', 'GásBrasil', 'Moinhos de Vento, Porto Alegre, RS', 1, 15),
-('23456789000191', 'WaterTech', 'Boa Viagem, Recife, PE', 4, 20),
+('23456789000191', 'WaterTech', 'Boa Viagem, Recife, PE', 4, 05),
 ('34567890000192', 'EcoH2O', 'Savassi, Belo Horizonte, MG', 2, 05),
-('45678901000193', 'GásLíder', 'Itaim Bibi, São Paulo, SP', 5, 90),
-('56789012000194', 'Suprimento Drink', 'Copacabana, Rio de Janeiro, RJ', 3, 10);
+('45678901000193', 'GásLíder', 'Itaim Bibi, São Paulo, SP', 5, 20),
+('56789012000194', 'Suprimento Drink', 'Copacabana, Rio de Janeiro, RJ', 3, 90);
 
 -- CONSULTANDO
 SELECT * FROM tb_company;
@@ -33,9 +33,7 @@ CREATE TABLE tb_supplier (
   cnpj CHAR(14),
   phone VARCHAR(20)
 );
-
-ALTER TABLE tb_supplier DROP COLUMN phone;
-ALTER TABLE tb_supplier ADD COLUMN phone VARCHAR(20); 
+ 
 -- ADICIONANDO DADOS
 INSERT INTO tb_supplier (id, supplied_product, name, cnpj, phone) VALUES 
 (20, 'Gás Natural', 'GásFórmula', '12345678901234', 8294567890),
@@ -43,7 +41,6 @@ INSERT INTO tb_supplier (id, supplied_product, name, cnpj, phone) VALUES
 (15, 'Gás de Cozinha', 'GásInfinity', '34567890123456', 9996789012),
 (05, 'Água Mineral', 'Minerale', '45678901234567', 9897890123), 
 (90, 'Bebidas', 'Cia das Bebidas', '56789012345678', 3198901245);
-
 
 -- CONSULTANDO
 SELECT * FROM tb_supplier;
@@ -55,14 +52,24 @@ CREATE TABLE tb_stock (
   supplier_id INT
 );
 
-
-DROP TABLE tb_stock;
 -- FOREIGN KEYS
 ALTER TABLE tb_stock ADD FOREIGN KEY (company_id) REFERENCES tb_company(id);
 ALTER TABLE tb_stock ADD FOREIGN KEY (supplier_id) REFERENCES tb_supplier(id);
 
 -- ADICIONANDO DADOS
-INSERT INTO tb_stock (capacity) VALUES('10000'), ('15000'), ('50000'), ('20000'), ('30000');
+INSERT INTO tb_stock (capacity) VALUES('10000'), ('15000'), ('50000'), ('20000'), ('30000'), ('18000');
+UPDATE tb_stock SET company_id = '1' WHERE id = 1;
+UPDATE tb_stock SET company_id = '4' WHERE id = 2;
+UPDATE tb_stock SET company_id = '1' WHERE id = 3;
+UPDATE tb_stock SET company_id = '2' WHERE id = 4;
+UPDATE tb_stock SET company_id = '5' WHERE id = 5;
+UPDATE tb_stock SET company_id = '3' WHERE id = 6;
+UPDATE tb_stock SET supplier_id = '15' WHERE id = 1;
+UPDATE tb_stock SET supplier_id = '15' WHERE id = 3;
+UPDATE tb_stock SET supplier_id = '90' WHERE id = 5;
+UPDATE tb_stock SET supplier_id = '20' WHERE id = 4;
+UPDATE tb_stock SET supplier_id = '10' WHERE id = 2;
+UPDATE tb_stock SET supplier_id = '5' WHERE id = 6;
 
 -- CONSULTANDO
 SELECT * FROM tb_stock;
@@ -73,14 +80,11 @@ CREATE TABLE tb_product (
   type_product VARCHAR(50),
   validity DATETIME,
   description VARCHAR(100),
-  stock_id INT,
-  transaction_id INT
+  stock_id INT
 ); 
 
 -- FOREIGN KEYS
 ALTER TABLE tb_product ADD FOREIGN KEY (stock_id) REFERENCES tb_stock(id);
-ALTER TABLE tb_product ADD FOREIGN KEY (transaction_id) REFERENCES tb_transaction(id);
-
 
 -- ADICIONANDO DADOS
 INSERT INTO tb_product (name, type_product, validity, description, stock_id) VALUES
@@ -89,7 +93,8 @@ INSERT INTO tb_product (name, type_product, validity, description, stock_id) VAL
 ('Gás', 'Gás de Cozinha', '2039-05-31','Botijão de gás, 5kg','3'),
 ('Água', 'Água Mineral', '2026-12-06','Galão de água sem gás, 20l', '4'),
 ('Coca-Cola', 'Refrigerante', '2025-01-10','Coca-cola retornável, 2l','5'),
-('Brahma', 'Cerveja', '2024-10-20','Engradado de latinhas, 6l','5');
+('Brahma', 'Cerveja', '2024-10-20','Engradado de latinhas, 6l','5'),
+('Água', 'Água com gás', '2026-12-18','Engradado de água com gás, 24l', '6');
 
 -- CONSULTANDO
 SELECT * FROM tb_product;
@@ -118,9 +123,17 @@ INSERT INTO tb_client (cpf, name, phone, location) VALUES
 ('95173468200', 'Mariana Rodrigues', '62984713500','Itaim Bibi, São Paulo, SP'),
 ('82615497330', 'Felipe Silva', '18959432600','Boa Viagem, Recife, PE');
 
+UPDATE tb_client SET transaction_id = '1' WHERE id = 1;
+UPDATE tb_client SET transaction_id = '2' WHERE id = 2;
+UPDATE tb_client SET transaction_id = '3' WHERE id = 3;
+UPDATE tb_client SET transaction_id = '4' WHERE id = 4;
+UPDATE tb_client SET transaction_id = '5' WHERE id = 5;
+UPDATE tb_client SET transaction_id = '6' WHERE id = 6;
+UPDATE tb_client SET transaction_id = '7' WHERE id = 7;
+UPDATE tb_client SET transaction_id = '8' WHERE id = 8;
+UPDATE tb_client SET transaction_id = '9' WHERE id = 9;
+UPDATE tb_client SET transaction_id = '10' WHERE id = 10;
 
-ALTER TABLE tb_client DROP COLUMN phone;
-ALTER TABLE tb_client ADD COLUMN phone VARCHAR(20); 
 -- CONSULTANDO
 SELECT * FROM tb_client;
 
@@ -159,6 +172,24 @@ INSERT INTO tb_transaction (value, date, form_payment, quantity_product, client_
 ('800.00', '2024-06-09', 'cartão de crédito','8','6','3'),
 ('450.00', '2024-06-09', 'cartão de crédito','3','7','1');
 
+UPDATE tb_transaction SET delivery_id = '1' WHERE id = 1;
+UPDATE tb_transaction SET delivery_id = '2' WHERE id = 2;
+UPDATE tb_transaction SET delivery_id = '3' WHERE id = 3;
+UPDATE tb_transaction SET delivery_id = '4' WHERE id = 4;
+UPDATE tb_transaction SET delivery_id = '5' WHERE id = 5;
+UPDATE tb_transaction SET delivery_id = '6' WHERE id = 6;
+UPDATE tb_transaction SET delivery_id = '7' WHERE id = 7;
+UPDATE tb_transaction SET delivery_id = '8' WHERE id = 8;
+UPDATE tb_transaction SET delivery_id = '9' WHERE id = 9;
+UPDATE tb_transaction SET delivery_id = '10' WHERE id = 10;
+UPDATE tb_transaction SET delivery_id = '11' WHERE id = 11;
+UPDATE tb_transaction SET delivery_id = '12' WHERE id = 12;
+UPDATE tb_transaction SET delivery_id = '13' WHERE id = 13;
+UPDATE tb_transaction SET delivery_id = '14' WHERE id = 14;
+UPDATE tb_transaction SET delivery_id = '15' WHERE id = 15;
+UPDATE tb_transaction SET delivery_id = '16' WHERE id = 16;
+UPDATE tb_transaction SET delivery_id = '17' WHERE id = 17;
+
 -- CONSULTANDO
 SELECT * FROM tb_transaction;
 
@@ -167,15 +198,13 @@ CREATE TABLE tb_delivery (
   status VARCHAR(20),
   delivery_location VARCHAR(50),
   delivery_date DATE,
-  transation_id INT
+  transaction_id INT
 );
 -- FOREIGN KEYS
-ALTER TABLE tb_delivery ADD FOREIGN KEY (transation_id) REFERENCES tb_transaction(id);
-ALTER TABLE tb_delivery DROP COLUMN status;
-ALTER TABLE tb_delivery ADD COLUMN status VARCHAR(20);
+ALTER TABLE tb_delivery ADD FOREIGN KEY (transaction_id) REFERENCES tb_transaction(id);
 
 -- ADICIONANDO DADOS
-INSERT INTO tb_delivery (status, delivery_location, delivery_date, transation_id) VALUES
+INSERT INTO tb_delivery (status, delivery_location, delivery_date, transaction_id) VALUES
 ('Pedido entregue', 'Moinhos de Vento, Porto Alegre, RS', '2024-05-25', '1'),
 ('Pedido entregue', 'Boa Viagem, Recife, PE', '2024-05-25', '2'),
 ('Pedido entregue', 'Savassi, Belo Horizonte, MG', '2024-05-26', '3'),
